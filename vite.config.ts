b465af4b7cf065ca3a6c14ccd8449a75187cb810
Mr.PAPI <thesnakewitcher@gmail.com>
2025-07-22 23:51:56 -0300

    chore: remove `base` in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/vesting-manager-ui",
+  base: "/",
   plugins: [react(),tailwindcss()],
 })

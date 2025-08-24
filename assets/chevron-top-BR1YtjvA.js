9fb13b63973cdc8ff01daad07bed6a4917b0cbd0
Mr.PAPI <thesnakewitcher@gmail.com>
2025-08-24 08:35:47 -0300

    Updates

--- assets/chevron-top-BR1YtjvA.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-Cr0zxmH5.js";import"./index-Ce7FnPKU.js";import"./index.es-C9F7oIOf.js";import"./events-DQ172AOg.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronTopSvg};
